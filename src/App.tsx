95866a105016016ca040b64e856d2937fbbfffce
PremSingh68749 <prems.wappnet@gmail.com>
2025-02-10 15:15:46 +0530

    react typescript template for project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App():React.ReactElement {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind CSS with TypeScript!</h1>
+    </div>
+  );
+}
+
+export default App;
